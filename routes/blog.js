3a193eaa288478ea2df49e87278c6564ada1c795
sarthak <nandini.srivastava.12.2004@gmail.com>
2023-06-23 02:17:55 +0530

    modified routes

--- routes/blog.js
+++ routes/blog.js
@@ -4,14 +4,14 @@ const router = express.Router();
 // Import Controller 
 const {createComment} = require("../controllers/commentController");
 const {createPost,getAllPosts} = require("../controllers/postController");
-const {likePost} = require("../controllers/likeController");
-
+const {likePost,unlikePost} = require("../controllers/likeController");
 
 // Mapping Create
 router.post("/comments/create",createComment)
 router.post("/posts/create",createPost)
 router.get("/posts",getAllPosts)
 router.post("/likes/like",likePost)
+router.post("/likes/unlike",unlikePost)
 
 // Export Controller
 module.exports = router;
